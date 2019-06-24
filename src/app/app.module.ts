5242abe4b99adcf7f8ab6daba8ce9130067f6864
Sebastien Avenet <avenet.seb@gmail.com>
2019-06-24 16:55:42 +0200

    create User component + custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { ExponentialPipe } from './exponential.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    ExponentialPipe
   ],
   imports: [
     BrowserModule
