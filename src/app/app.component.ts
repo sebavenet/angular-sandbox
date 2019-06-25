9df1c950228cf374f497d127c1d1173d366994f1
Sebastien Avenet <avenet.seb@gmail.com>
2019-06-25 09:51:56 +0200

    two-way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'My Great Project';
-  dates = [new Date().getTime()];
-  addDate() {
-    // Transformation d'objet => Angular ne detectera pas de changements
-    this.dates.push(new Date().getTime());
-    // OU
-    // Nouvelle référence d'objet => Angular detectera le changement
-    // this.dates = this.dates.concat(new Date().getTime());
-  }
 }
