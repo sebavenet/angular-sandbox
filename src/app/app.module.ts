4f1181d6291d2cc75cb6bcfb4af0d85d8609c3f5
Sebastien Avenet <avenet.seb@gmail.com>
2019-06-25 12:05:56 +0200

    clean workspace before next step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
-import { HighlightDirective } from './highlight.directive';
-import { InputMaxLengthDirective } from './input-max-length.directive';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    HighlightDirective,
-    InputMaxLengthDirective,
     UserDetailComponent
   ],
   imports: [
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from './user';
-import { UserDetailComponent } from './user-detail/user-detail.component';
-import { ParentChildInteractionService } from '../parent-child-interaction.service';
-import { Subscription } from 'rxjs';
 
 export const USERS: User[] = [
   {
     id: 1,
-    username: 'mehdi',
+    username: 'test',
     password: 'pa$$word',
     email: 'mehdi.mecheri@viveris.fr',
     firstname: 'Mehdi',
@@ -16,7 +13,7 @@ export const USERS: User[] = [
   },
   {
     id: 2,
-    username: 'leo',
+    username: 'test',
     password: 'pa$$word',
     email: 'lionel.messi@barca.es',
     firstname: 'Lionel',
@@ -25,7 +22,7 @@ export const USERS: User[] = [
   },
   {
     id: 3,
-    username: 'cristiano',
+    username: 'test',
     password: 'pa$$word',
     email: 'cristiano.ronaldo@real.es',
     firstname: 'Cristiano',
@@ -34,7 +31,7 @@ export const USERS: User[] = [
   },
   {
     id: 4,
-    username: 'neymar',
+    username: 'test',
     password: 'pa$$word',
     email: 'neymar.jr@psg.fr',
     firstname: 'Neymar',
@@ -50,41 +47,18 @@ export const USERS: User[] = [
 })
 export class UserComponent implements OnInit {
 
-  image = 'https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f471-1f3fb.png';
-  image2 = '               https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f471-1f3fb.png';
   users: User[];
   selectedUser: User;
 
-  @ViewChild(UserDetailComponent, null) ud: UserDetailComponent;
+  image = 'https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets/png/1f471-1f3fb.png';
 
-  subscription: Subscription;
-
-  constructor(private service: ParentChildInteractionService) {
-    this.subscription = service.broadcastChildStream$.subscribe((messageFromChild: string) => console.log(messageFromChild));
-  }
+  constructor() { }
 
   ngOnInit() {
     this.users = USERS;
   }
 
-  ngOnDestroy() {
-    // do no forget to implement OnDestroy interface
-    this.subscription.unsubscribe(); // prevent memory leak
-  }
-
   onSelect(user: User): void {
     this.selectedUser = user;
-    if (this.ud) {
-      console.log(this.ud.hello);
-    }
   }
-
-  onActionFromUserDetail(msg: string) {
-    alert(msg);
-  }
-
-  broadcastParent() {
-    this.service.broadcastParent('Hello from parent');
-  }
-
 }
