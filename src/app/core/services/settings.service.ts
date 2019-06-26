25fd48d8da592f56071eb7476a35daf327edff3d
Sebastien Avenet <avenet.seb@gmail.com>
2019-06-26 17:20:34 +0200

    play with different environments

--- src/app/core/services/settings.service.ts
+++ src/app/core/services/settings.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import * as Hjson from 'hjson';
+import { environment } from 'src/environments/environment';
 
 /**
  * App config interface
@@ -53,7 +54,7 @@ export class SettingsService {
       .append('Pragma', 'no-cache');
 
     return new Promise((resolve, reject) => {
-      this.http.get(`assets/resources/settings.hjson`, { headers: headers, responseType: 'text' })
+      this.http.get(`assets/resources/settings.${ environment.envName }.hjson`, { headers: headers, responseType: 'text' })
         .pipe(catchError((error) => { throw error; })).subscribe((res_data) => {
           resolve(res_data);
           this._config = Hjson.parse(res_data);
